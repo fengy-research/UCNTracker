cdae9b7b73b9b8f57754ad6e33e7e361812754f6
Feng Yu <fengyu@dhcp5-240.iucf.indiana.edu>
2009-05-19 21:44:28 -0400

    autogen in libyaml-glib - or it won't bootstrap with autoreconf on RHEL5!

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,6 @@ autoheader
 automake --add-missing
 autoconf
 (cd libyaml; autoreconf -fvi;)
-(cd libyaml-glib; autoreconf -fvi;)
+(cd libyaml-glib; ./autogen.sh --no-configure $*;)
 ./configure --enable-maintainer-mode $*
 
